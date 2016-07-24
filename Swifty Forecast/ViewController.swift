ae829240f90162b66d141d30eeced2cba9f015c4
hexadecymalny <milk.pawel@gmail.com>
2016-07-24 17:31:53 -0500

    Init commit

---
+++ Swifty Forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swifty Forecast
+//
+//  Created by Pawel Milek on 24/07/16.
+//  Copyright © 2016 Pawel Milek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
