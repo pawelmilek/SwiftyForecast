fe67c11530deb33b2a0e340718ee057e16eda6a6
Pawel Milek <pawel.milek0626@gmail.com>
2024-07-03 13:12:10 -0500

    Create Theme Feature dev app

---
+++ DevApps/ThemeDevApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ThemeDevApp
+//
+//  Created by Pawel Milek on 7/3/24.
+//  Copyright © 2024 Pawel Milek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevApps/ThemeDevApp/ThemeDevAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeDevAppApp.swift
+//  ThemeDevApp
+//
+//  Created by Pawel Milek on 7/3/24.
+//  Copyright © 2024 Pawel Milek. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ThemeDevAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
